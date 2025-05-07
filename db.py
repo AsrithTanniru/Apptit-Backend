7a13dfe1e96880f702a44c91184a2f455a5a4755
asrith <asrithtanniru155@gmail.com>
2025-05-07 12:02:55 +0530

    Update env

--- db.py
+++ db.py
@@ -4,9 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Test%40123@localhost:5432/fastapi'
-
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionMaker = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
